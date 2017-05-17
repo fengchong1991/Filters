8a74fb0742c829fe5d4f773616a4e4f71ed83317
Chong Feng <chong.feng@adelphi.digital>
2017-05-17 23:07:41 +1000

    First commit

---
+++ Filters/Controllers/GoogleAccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Controllers
+{
+    public class GoogleAccountController : Controller
+    {
+        // GET: GoogleAccount
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password, string returnUrl)
+        {
+            if (username.EndsWith("@google.com") && password == "secret")
+            {
+                FormsAuthentication.
+            }
+        }
+    }
+}
---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Filters.Infrastructure;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //[CustomAuth(true)]
+        [Authorize(Users="admin")]
+        public ActionResult Index() => View("Message", "This is the Index action on the Home controller");
+    }
